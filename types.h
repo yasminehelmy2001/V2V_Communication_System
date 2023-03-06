0dc4c66e015b721e593c3f457e3920b3ed0be368
yasminehelmy2001 <114500152+yasminehelmy2001@users.noreply.github.com>
2023-03-07 00:09:15 +0200

    Delete types.h

--- types.h
+++
@@ -1,8 +0,0 @@
-typedef unsigned char u8;
-typedef unsigned short int*    pu16;
-typedef unsigned short int u16;
-typedef unsigned long int u32;
-typedef signed char s8;
-typedef signed short int s16;
-typedef signed int s32;
-typedef float f32;
